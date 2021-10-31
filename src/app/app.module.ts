46ce047dbad0686687c9edf07524fe1c0bc27ad5
Leonardo Castro <castroleonardo@live.cl>
2021-10-31 20:53:49 -0300

    proyecto arreglado, con estilos

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListadoDeHeroesComponent } from './components/listado-de-heroes/listado-de-heroes.component';
+import { HeroesService } from './services/heroes.service';
+import { HeroProfileComponent } from './components/hero-profile/hero-profile.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { ModalPollComponent } from './components/modal-poll/modal-poll.component';
+import { CapitalizePipe } from './directives/capitalize.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListadoDeHeroesComponent,
+    HeroProfileComponent,
+    SpinnerComponent,
+    ModalPollComponent,
+    CapitalizePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [HeroesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/listado-de-heroes/listado-de-heroes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-listado-de-heroes',
+  templateUrl: './listado-de-heroes.component.html',
+  styleUrls: ['./listado-de-heroes.component.css']
+})
+export class ListadoDeHeroesComponent implements OnInit {
+
+  public title = 'Tutorial de Angular - Héroes de Marvel';
+  public searchString;
+  // The child component : spinner
+  @ViewChild('spi') spinner;
+  /* public heroes: Array<Heroe> = []; */
+
+  constructor(public heroesService: HeroesService, private router:Router) { }
+
+  submitSearch() {
+    this.heroesService.resetPager();
+    this.heroesService.getHeroes(this.searchString);
+  }
+
+  prevPage() {
+    this.heroesService.getHeroes(this.searchString, this.heroesService.page - 1);
+  }
+    
+  nextPage() {
+    this.heroesService.getHeroes(this.searchString, this.heroesService.page + 1);
+  }
+
+  go_to(id){
+    this.router.navigateByUrl('/heroe/'+id);
+  }
+
+  ngOnInit() {
+    this.heroesService.getHeroes();
+  }
+
+}
