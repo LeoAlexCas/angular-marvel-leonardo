482c56a99db6a7614a3e0c7294dd82389c9f98a5
Leonardo Castro <castroleonardo@live.cl>
2021-11-01 20:44:28 -0300

    iniciando testing

--- src/app/components/hero-profile/hero-profile.component.spec.ts
+++ src/app/components/hero-profile/hero-profile.component.spec.ts
@@ -1,4 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, ComponentFixtureAutoDetect, TestBed } from '@angular/core/testing';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { heroes } from 'src/app/store/marvel.selector';
 
 import { HeroProfileComponent } from './hero-profile.component';
 
@@ -6,9 +8,34 @@ describe('HeroProfileComponent', () => {
   let component: HeroProfileComponent;
   let fixture: ComponentFixture<HeroProfileComponent>;
 
+  class HeroMock {
+    getHeroeMock() {
+      return [{
+        id: 1011176,
+        name: 'Ajak',
+        description: '',
+        resourceURI: 'http://gateway.marvel.com/v1/public/characters/1011176',
+        teamColor: '',
+        modified: '1969-12-31T19:00:00-0500',
+        thumbnail: {
+          path: 'http://i.annihil.us/u/prod/marvel/i/mg/2/80/4c002f35c5215',
+          extension: 'jpg'
+        }
+      }];
+    }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroProfileComponent ]
+      declarations: [ HeroProfileComponent ],
+      providers: [
+        {
+          provide: ComponentFixtureAutoDetect,
+        },
+        {
+          provide: HeroesService, useClass: HeroMock
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +49,8 @@ describe('HeroProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get heroes', () => {
+    
+  })
 });
